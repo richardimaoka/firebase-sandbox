093a56f13c8d6d8344542e02749eaf1578de3d6e
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-11-14 21:59:07 +0900

    mo stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,26 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: "./src/index.js",
   devtool: "eval-source-map",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
